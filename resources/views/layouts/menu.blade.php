07c07b2126e36ea57a4df5e64fccf6ca1b4caea8
Biswajit Singha <biswajit.singha@codepoetry.in>
2021-09-01 18:01:38 +0530

    testProject

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,15 @@
+<!-- need to remove -->
+<li class="nav-item">
+    <a href="{{ route('home') }}" class="nav-link active">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Home</p>
+    </a>
+    <a href="" class="nav-link ">
+        <i class="nav-icon fas fa-home"></i>
+        User
+    </a>
+    <a href="xyz.com" class="nav-link ">
+        <i class="nav-icon fas fa-home"></i>
+        App Setting
+    </a>
+</li>
